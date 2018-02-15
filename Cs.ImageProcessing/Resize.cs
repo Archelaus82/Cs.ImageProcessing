9be04b50bbf6391b7d7adabdea460c9d2aaed57d
Josh Holzworth <holzworthjosh@gmail.com>
2018-02-14 21:43:02 -0800

    Initial add

---
+++ Cs.ImageProcessing/Resize.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Cs.ImageProcessing
+{
+    public class Resize
+    {
+        private static void ResizeAlgorithm(out int newWidth, out int newHeight, out int posX, out int posY,
+            int width, int height, int imageWidth, int imageHeight)
+        {
+            double ratioX = (double)width / (double)imageWidth;
+            double ratioY = (double)height / (double)imageHeight;
+            double ratio = ratioX < ratioY ? ratioX : ratioY;
+
+            newWidth = Convert.ToInt32(imageWidth * ratio);
+            newHeight = Convert.ToInt32(imageHeight * ratio);
+            posX = Convert.ToInt32((width - (imageWidth * ratio)) / 2);
+            posY = Convert.ToInt32((height - (imageHeight * ratio)) / 2);
+        }
+
+        /// <summary>
+        /// Resizes an image
+        /// </summary>
+        /// <param name="path">path to file</param>
+        /// <param name="originalFile">file to resize</param>
+        /// <param name="width">desired width</param>
+        /// <param name="height">desired height</param>
+        /// <param name="format">image format</param>
+        /// <returns>returns string equivalent to image</returns>
+        public static String64 ResizeImage(string path, string originalFile, int width, int height, ImageFormat format)
+        {
+            Image image = Image.FromFile(originalFile);
+            int imageWidth = image.Width;
+            int imageHeight = image.Height;
+
+            System.Drawing.Image thumbnail = new Bitmap(width, height);
+            System.Drawing.Graphics graphic = System.Drawing.Graphics.FromImage(thumbnail);
+
+            graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphic.SmoothingMode = SmoothingMode.HighQuality;
+            graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            graphic.CompositingQuality = CompositingQuality.HighQuality;
+
+            int newWidth;
+            int newHeight;
+            int posX;
+            int posY;
+            ResizeAlgorithm(out newWidth, out newHeight, out posX, out posY, width, height, imageWidth, imageHeight);
+
+            graphic.Clear(Color.Transparent);
+            graphic.DrawImage(image, posX, posY, newWidth, newHeight);
+
+            System.Drawing.Imaging.ImageCodecInfo[] info = ImageCodecInfo.GetImageEncoders();
+            EncoderParameters encoderParameters;
+            encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 100L);
+            //string saveFile = String.Format("{0}thumbnail_{1}.bmp", path, Path.GetFileNameWithoutExtension(originalFile));
+            //thumbnail.Save(saveFile, info[1], encoderParameters);
+
+            return ConvertToByteArray(thumbnail, format);
+        }
+
+        public static String64 ConvertToByteArray(Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                byte[] data = ms.ToArray();
+
+                return (String64)Convert.ToBase64String(data);
+            }
+        }
+
+        public static Image ToImage(String64 str)
+        {
+            byte[] data = Convert.FromBase64String((string)str);
+            MemoryStream ms = new MemoryStream(data, 0, data.Length);
+            ms.Write(data, 0, data.Length);
+            Image image = Image.FromStream(ms, true);
+
+            return image;
+        }
+    }
+}
---
+++ Cs.ImageProcessing/String64.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Cs.ImageProcessing
+{
+    public class String64
+    {
+        private string _str;
+
+        public String64(string str) 
+        { 
+            this._str = str.IsBase64Encoded() ? str : str.Base64Encode(); 
+        }
+
+        public static explicit operator string(String64 str) { return str._str; }
+        public static explicit operator String64(string str) { return new String64(str); }
+
+        public string Decode { get { return ((string)this._str).Base64Decode(); } }
+    }
+
+    public static class Extensions
+    {
+        public static string Base64Encode(this string utf16str)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(utf16str);
+            return Convert.ToBase64String(data);
+        }
+
+        public static string Base64Decode(this string utf64str)
+        {
+            byte[] data = Convert.FromBase64String(utf64str);
+            return Encoding.Unicode.GetString(data);
+        }
+
+        public static bool IsBase64Encoded(this string str)
+        {
+            const string REGEX_PATTERN = @"^[a-zA-Z0-9\+/]*={0,3}$";
+
+            str = str.Trim();
+            return (str.Length % 4 == 0) & Regex.IsMatch(str, REGEX_PATTERN);
+        }
+    }
+}
